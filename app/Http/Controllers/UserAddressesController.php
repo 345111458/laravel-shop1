9d86f14c903869e292d910bf8fbdb9c4cf753c18
vagrant <vagrant@homestead>
2019-07-17 08:26:06 +0000

    fdfdf

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -16,7 +16,7 @@ class UserAddressesController extends Controller
         ]);
     }
 
-
+    //添加收货 提交地址
     public function store(UserAddressRequest $request){
 
         $request->user()->addresses()->create($request->only([
@@ -32,9 +32,44 @@ class UserAddressesController extends Controller
     }
 
 
+    // 添加收货
     public function create(){
 
         return view('user_addresses.create_and_edit',['address'=> new UserAddress()]);
+    }
+
+
+    // 修改收货地址
+    public function edit(UserAddress $user_address){
+        $this->authorize('own' , $user_address);
+
+        return view('user_addresses.create_and_edit',['address'=>$user_address]);
+    }
+
+
+    //修改提交地址
+    public function update(UserAddress $user_address , UserAddressRequest $request){
+        $this->authorize('own' , $user_address);
+
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+        return redirect()->route('user_addresses.index');
+    }
+
+
+    public function destroy(UserAddress $user_address){
+        $this->authorize('own' , $user_address);
+        $user_address->delete();
+
+        return [];
+        return redirect()->route('user_addresses.index');
     }
 
 
